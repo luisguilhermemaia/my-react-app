c6719b72dd0f27bf85ead1bebfb93a990a21dedd
guilherme <guilherme@petcoach.co>
2019-02-14 10:42:21 -0300

    Some refactoring and navigation logic

--- src/modules/user/Login.js
+++ src/modules/user/Login.js
@@ -1,74 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import { Field, ErrorMessage, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
 
-const Login = () => (
-  <Formik
-    initialValues={{ email: '', password: '' }}
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }, 500);
-    }}
-    validationSchema={Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required('Required'),
-      password: Yup.string()
-        .min(4)
-        .max(8)
-        .required('Required'),
-    })}
-  >
-    {props => {
-      const {
-        isSubmitting,
-        handleSubmit,
-      } = props;
-      return (
-        <form onSubmit={handleSubmit}>
-         <label htmlFor="email">
-            Email
-          </label>
-          <Field
-            id="email"
-            name="email"
-            type="text"
-            placeholder="Enter your email"
-          />
-          <div className="input-feedback">
-            <ErrorMessage name="email" />
-          </div>
+const initialValues = {
+  email: '',
+  password: '',
+};
 
-          <label htmlFor="password">
-            Password
-          </label>
-          <Field
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Enter your password"
-          />
-          <div className="input-feedback">
-            <ErrorMessage name="password" />
-          </div>
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-          <div className="bottom-register">
-            <p>
-              Not yet a user?&emsp;
-                <Link to="/register">
-                  Register now!
-                </Link>
-            </p>
-          </div>
-        </form>
-      );
-    }}
-  </Formik>
-);
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email()
+    .required('Required'),
+  password: Yup.string()
+    .min(4)
+    .max(8)
+    .required('Required'),
+});
+class Login extends Component {
+  handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+      this.props.history.push('/posts');
+    }, 500);
+  }
 
-export default Login;
+  render() {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={this.handleSubmit}
+      >
+        {props => {
+          const {
+            isSubmitting,
+            handleSubmit,
+          } = props;
+          return (
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="email">
+                Email
+              </label>
+              <Field
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+              />
+              <div className="input-feedback">
+                <ErrorMessage name="email" />
+              </div>
+
+              <label htmlFor="password">
+                Password
+              </label>
+              <Field
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+              />
+              <div className="input-feedback">
+                <ErrorMessage name="password" />
+              </div>
+
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+
+              <div className="bottom-register">
+                <p>
+                  Not yet a user?&emsp;
+                  <Link to="/register">
+                      Register now!
+                  </Link>
+                </p>
+              </div>
+            </form>
+          );
+        }}
+      </Formik>
+    );
+  }
+}
+
+export default withRouter(Login);
--- src/modules/user/Register.js
+++ src/modules/user/Register.js
@@ -1,112 +1,129 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import { Field, ErrorMessage, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
 import PictureUploader from './components/picture-uploader/PitctureUploader';
 
-const Register = () => (
-  <Formik
-    initialValues={{ email: '', password: '', fullName: '', dateOfBirth: '', avatar: '' }}
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }, 500);
-    }}
-    validationSchema={Yup.object().shape({
-      fullName: Yup.string()
-        .required('Required'),
-      email: Yup.string()
-        .email()
-        .required('Required'),
-      password: Yup.string()
-        .min(4)
-        .max(8)
-        .required('Required'),
-      dateOfBirth: Yup.date()
-        .required('Required'),
-    })}
-  >
-    {props => {
-      const {
-        values,
-        isSubmitting,
-        handleSubmit,
-        handleChange,
-      } = props;
-      return (
-        <form onSubmit={handleSubmit}>
-          <PictureUploader
-            value={values.avatar}
-            onChange={handleChange('avatar')}
-          />
-          <label htmlFor="fullName">
-            Full name
-          </label>
-          <Field
-            id="fullName"
-            name="fullName"
-            type="text"
-            placeholder="Enter your full name"
-          />
-          <div className="input-feedback">
-            <ErrorMessage name="email" />
-          </div>
+const initialValues = {
+  email: '',
+  password: '',
+  fullName: '',
+  dateOfBirth: '',
+  avatar: ''
+};
 
-          <label htmlFor="email">
-            Email
-          </label>
-          <Field
-            id="email"
-            name="email"
-            type="text"
-            placeholder="Enter your email"
-          />
-          <div className="input-feedback">
-            <ErrorMessage name="email" />
-          </div>
+const validationSchema = Yup.object().shape({
+  fullName: Yup.string()
+    .required('Required'),
+  email: Yup.string()
+    .email()
+    .required('Required'),
+  password: Yup.string()
+    .min(4)
+    .max(8)
+    .required('Required'),
+  dateOfBirth: Yup.date()
+    .required('Required'),
+});
+class Register extends Component {
+  handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+      this.props.history.push('/posts');
+    }, 500);
+  }
 
-          <label htmlFor="password">
-            Password
-          </label>
-          <Field
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Enter your password"
-          />
-          <div className="input-feedback">
-            <ErrorMessage name="password" />
-          </div>
+  render() {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={this.handleSubmit}
+      >
+        {props => {
+          const {
+            values,
+            isSubmitting,
+            handleSubmit,
+            handleChange,
+          } = props;
+          return (
+            <form onSubmit={handleSubmit}>
+              <PictureUploader
+                value={values.avatar}
+                onChange={handleChange('avatar')}
+              />
+              <label htmlFor="fullName">
+                Full name
+              </label>
+              <Field
+                id="fullName"
+                name="fullName"
+                type="text"
+                placeholder="Enter your full name"
+              />
+              <div className="input-feedback">
+                <ErrorMessage name="fullName" />
+              </div>
 
-          <label htmlFor="dateOfBirth">
-            Birth date
-          </label>
-          <Field
-            id="dateOfBirth"
-            name="dateOfBirth"
-            type="date"
-            placeholder="Enter your birth date"
-          />
-          <div className="input-feedback">
-            <ErrorMessage name="dateOfBirth" />
-          </div>
+              <label htmlFor="email">
+                Email
+              </label>
+              <Field
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+              />
+              <div className="input-feedback">
+                <ErrorMessage name="email" />
+              </div>
 
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-          <div className="bottom-register">
-            <p>
-              Already a user?&emsp;
+              <label htmlFor="password">
+                Password
+              </label>
+              <Field
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+              />
+              <div className="input-feedback">
+                <ErrorMessage name="password" />
+              </div>
+
+              <label htmlFor="dateOfBirth">
+                Birth date
+              </label>
+              <Field
+                id="dateOfBirth"
+                name="dateOfBirth"
+                type="date"
+                placeholder="Enter your birth date"
+              />
+              <div className="input-feedback">
+                <ErrorMessage name="dateOfBirth" />
+              </div>
+
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+
+              <div className="bottom-register">
+                <p>
+                  Already a user?&emsp;
                 <Link to="/login">
-                Sign in
+                    Sign in
                 </Link>
-            </p>
-          </div>
-        </form>
-      );
-    }}
-  </Formik>
-);
+                </p>
+              </div>
+            </form>
+          );
+        }}
+      </Formik>
+    );
+  }
+}
 
-export default Register;
+export default withRouter(Register);
