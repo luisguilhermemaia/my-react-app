6efa4760989be755d4b3195c28df051f6fe21262
guilherme <guilherme@petcoach.co>
2019-02-15 10:19:56 -0300

    Create container for the app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
-
+import styled from 'styled-components';
 import Login from './modules/user/Login'
 import Register from './modules/user/Register'
 import Posts from './modules/posts/Posts'
 
-import './App.css';
+const Page = styled.div`
+  align-items: center;
+  background-color: rgb(98,104,189);
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 1.5rem;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <Page>
         <Router>
           <Switch>
             <Route exact path="/login" component={Login} />
@@ -22,7 +29,7 @@ class App extends Component {
             <Route path="/posts" component={Posts} />
           </Switch>
         </Router>
-      </div >
+      </Page>
     )
   }
 }
