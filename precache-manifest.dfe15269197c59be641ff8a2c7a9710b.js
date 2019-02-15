2c74d8aa93af2bdbbcbaf82cb6e2a0ad684841cb
guilherme <guilherme@petcoach.co>
2019-02-15 16:28:49 -0300

    Updates

---
+++ precache-manifest.dfe15269197c59be641ff8a2c7a9710b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd53da3d3780ffd15c6872890144e4bf",
+    "url": "/my-react-app/static/media/user.cd53da3d.svg"
+  },
+  {
+    "revision": "d3fdce2ff13cb3fde19910ee8c6c03af",
+    "url": "/my-react-app/static/media/logo.d3fdce2f.svg"
+  },
+  {
+    "revision": "5a3a79d249b368d6ccbd8d3f1b4519ca",
+    "url": "/my-react-app/static/media/lock.5a3a79d2.svg"
+  },
+  {
+    "revision": "74eff99cea11754c8867",
+    "url": "/my-react-app/static/js/runtime~main.74eff99c.js"
+  },
+  {
+    "revision": "18111c79cf3e3884f361",
+    "url": "/my-react-app/static/js/main.18111c79.chunk.js"
+  },
+  {
+    "revision": "60cd1f5f9a1a15405205",
+    "url": "/my-react-app/static/js/2.60cd1f5f.chunk.js"
+  },
+  {
+    "revision": "18111c79cf3e3884f361",
+    "url": "/my-react-app/static/css/main.3caec8ca.chunk.css"
+  },
+  {
+    "revision": "63c1abdf7bdda9fc67dd65ebd5611257",
+    "url": "/my-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.320bcb4402cd8e02c73abb5d11336c41.js"
+  "/my-react-app/precache-manifest.dfe15269197c59be641ff8a2c7a9710b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
