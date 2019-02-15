77ab6d6f0dcf5788aba290e22a71be13d8f47025
guilherme <guilherme@petcoach.co>
2019-02-15 11:13:22 -0300

    Create global style for the background color

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Login from './modules/user/Login'
 import Register from './modules/user/Register'
 import Posts from './modules/posts/Posts'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgb(98,104,189);
+  }
+`;
+
 const Page = styled.div`
   align-items: center;
-  background-color: rgb(98,104,189);
   display: flex;
   justify-content: center;
   min-height: 100vh;
@@ -21,15 +26,18 @@ const Page = styled.div`
 class App extends Component {
   render() {
     return (
-      <Page>
-        <Router>
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/posts" component={Posts} />
-          </Switch>
-        </Router>
-      </Page>
+      <React.Fragment>
+        <GlobalStyle />
+        <Page>
+          <Router>
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/posts" component={Posts} />
+            </Switch>
+          </Router>
+        </Page>
+      </React.Fragment>
     )
   }
 }
