221ef0df2e55cea002e5579d3d2e13f7c5fedad1
guilherme <guilherme@petcoach.co>
2019-02-15 15:28:52 -0300

    Updates

---
+++ precache-manifest.1a66bf4018d02b8236dcff2867242468.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd53da3d3780ffd15c6872890144e4bf",
+    "url": "/static/media/user.cd53da3d.svg"
+  },
+  {
+    "revision": "d3fdce2ff13cb3fde19910ee8c6c03af",
+    "url": "/static/media/logo.d3fdce2f.svg"
+  },
+  {
+    "revision": "5a3a79d249b368d6ccbd8d3f1b4519ca",
+    "url": "/static/media/lock.5a3a79d2.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "89cbecb4069b06c57ae2",
+    "url": "/static/js/main.89cbecb4.chunk.js"
+  },
+  {
+    "revision": "053bb3241353223980ab",
+    "url": "/static/js/2.053bb324.chunk.js"
+  },
+  {
+    "revision": "89cbecb4069b06c57ae2",
+    "url": "/static/css/main.3caec8ca.chunk.css"
+  },
+  {
+    "revision": "bd1645115bae409f3e0c4d99d25e9f83",
+    "url": "/index.html"
+  }
+];
